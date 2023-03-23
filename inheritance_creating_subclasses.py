85d9e221d559fd6eb9f52ce8c0e4707b882bfcc5
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-23 20:23:33 -0300

    inheritance subclasses

--- inheritance_creating_subclasses.py
+++ inheritance_creating_subclasses.py
@@ -15,8 +15,51 @@ class Employee:
     def apply_raise(self):
         self.pay =int(self.pay * self.raise_amount)
 
-dev_1 = Employee('Ami','Cabrera',50000)
-dev_2 = Employee('Adel','Cabrera',60000)
+class Developer(Employee):
+    raise_amount =1.10
 
-print(dev_1.email)
-print(dev_2.email)
+    def __init__(self,first,last,pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+class Manager(Employee):
+    def __init__(self,first,last,pay, Employee=None):
+        super().__init__(first, last, pay)
+        if Employee is None:
+            self.Employee =[]
+        else:
+            self.Employee = Employee
+    def add_emp(self, emp):
+        if emp not in self.Employee:
+            self.Employee.append(emp)
+    def remove_emp(self, emp):
+        if emp in self.Employee:
+            self.Employee.remove(emp)
+    def print_emps(self):
+        for emp in self.Employee:
+            print('-->', emp.fullname())
+
+
+dev_1 = Developer('Ami','Cabrera',50000, "Python")
+dev_2 = Developer('Adel','Cabrera',60000, "Java")
+
+mgr_1 = Manager('Tyler', 'Durden', 90000,[dev_1])
+
+print(mgr_1.email)
+
+mgr_1.add_emp(dev_2)
+mgr_1.remove_emp(dev_1)
+
+mgr_1.print_emps()
+
+print(isinstance(mgr_1,Manager))#para ver si pertenece a una instancia de clase (instancia sospechosa,clase )
+print(issubclass(Developer,Employee))
+
+
+
+
+
+#print(help(Developer)) PARA VER LA CADENA DE CLASES Y SUS FUNCIONES DENTRO
+
+
+
