8512d15b071138a506e5a5795c5bdecfe73aea1d
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-22 21:47:29 -0300

    first commit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,12 @@
-
-for number in range(1,101):
-    if number % 3 == 0 and number % 5 == 0:
-        print("fizzbuzz")
-    elif number % 3 == 0:
-        print("fizz")
-    elif number% 5 ==0:
-        print("buzz")
-    else:
-        print(number)
-    
+def fizz_Buzz(n):
+    for i in range(1, n +1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+        elif i % 3 == 0:
+            print("fizz")
+        elif i % 5 ==0:
+            print("buzz")
+        else:
+            print(i)
+print(fizz_Buzz(101))
+            
