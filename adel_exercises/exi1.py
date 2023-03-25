a5da90688ef6450461187ce55abb6aaca3a165f8
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-25 15:22:29 -0300

    reversed list ex

--- adel_exercises/exi1.py
+++ adel_exercises/exi1.py
@@ -1,3 +1,11 @@
 # Escribir un programa que almacene en una lista los números del 1 al 10 
 # y los muestre por pantalla en orden inverso separados por comas.
 
+my_list=[1,2,3,4,5,6,7,8,9,10]
+
+my_list.reverse()
+
+print(my_list)
+
+
+
