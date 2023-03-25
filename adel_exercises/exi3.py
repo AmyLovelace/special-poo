d2072f179b0755969e01be84d0a9fb87cfe2e174
amanecer <amanecer.luz.cabrera@gmail.com>
2023-03-25 16:00:34 -0300

    function print greet

---
+++ adel_exercises/exi3.py
@@ -0,0 +1,9 @@
+#Escribir una función a la que se le pase una cadena <nombre> y muestre por pantalla el saludo ¡hola <nombre>!.
+
+
+def holis():
+    nombre = input("¿cual es tu nombre?\n")
+    print("hola " + nombre + " encantado conocerte")
+     
+holis()
+
